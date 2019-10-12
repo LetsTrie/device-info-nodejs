dbfb35088aa835245dd64237650ec68708e8849e
letstrie <sakibkhan111296@gmail.com>
2019-10-12 19:53:04 +0600

    Checking Heroku

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const geoip = require('geoip-lite');
 const ua = require('ua-parser-js');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  return res.json({
+  console.log(ua(req.headers["user-agent"]));
+  res.render('hello', {
     headers: req.headers,
     ip: req.ip,
     deviceInfo: ua(req.headers["user-agent"])
   });
+});
+app.get('/hello', (req, res) => {
 })
 
 app.listen(3000);
